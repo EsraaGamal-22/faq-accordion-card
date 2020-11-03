2ace94717920210a7dc7f23df09f098d98aa80d4
esraagamal <esraagamal506@gmail.com>
2020-11-03 03:35:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ function showAns(x){
     var ans=document.getElementsByClassName("answer");
     if(arrow.style.transform=='rotate(180deg)')
     {
-        console.log("if: x-1: "+ (x-1));
         arrow.style.transform='rotate(360deg)';
         ans[x-1].style.display="block";
         que[x-1].style.fontSize = "larger";
         que[x-1].style.fontWeight = "700";
         que[x-1].style.color="black";
-        console.log(ans[x]);  
     }
     else{
         arrow.style.transform='rotate(180deg)';
