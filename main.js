06b9a8a80f447ebf8b4237593964aaf745507023
esraagamal <esraagamal506@gmail.com>
2020-11-04 04:04:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 function showAns(x){
-    var arrow=document.getElementById(x);
+    var arrow=document.getElementsByClassName("arrow-icon");
     var que=document.getElementsByClassName("Q-content");
     var ans=document.getElementsByClassName("answer");
-    if(arrow.style.transform=='rotate(180deg)')
+    console.log(window.getComputedStyle(ans[x-1]).display);
+
+    if(window.getComputedStyle(ans[x-1]).display == 'none' )
     {
-        arrow.style.transform='rotate(360deg)';
+
+        arrow[x-1].style.transform='rotate(360deg)';
         ans[x-1].style.display="block";
-        que[x-1].style.fontSize = "larger";
         que[x-1].style.fontWeight = "700";
         que[x-1].style.color="black";
     }
     else{
-        arrow.style.transform='rotate(180deg)';
+        arrow[x-1].style.transform='rotate(180deg)';
         ans[x-1].style.display="none";
-        que[x-1].style.fontSize = "unset";
         que[x-1].style.fontWeight = "400";
         que[x-1].style.color="unset";
 
