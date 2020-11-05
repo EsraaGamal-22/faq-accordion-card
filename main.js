e2867dc5ddd0301cf31e670e6d000bf0f4fe629a
esraagamal <esraagamal506@gmail.com>
2020-11-05 02:22:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ function showAns(x){
     var arrow=document.getElementsByClassName("arrow-icon");
     var que=document.getElementsByClassName("Q-content");
     var ans=document.getElementsByClassName("answer");
-    console.log(window.getComputedStyle(ans[x-1]).display);
-
     if(window.getComputedStyle(ans[x-1]).display == 'none' )
     {
 
         arrow[x-1].style.transform='rotate(360deg)';
-        ans[x-1].style.display="block";
+        ans[x-1].classList.add("active");
         que[x-1].style.fontWeight = "700";
         que[x-1].style.color="black";
     }
     else{
         arrow[x-1].style.transform='rotate(180deg)';
-        ans[x-1].style.display="none";
+        ans[x-1].classList.remove("active");
         que[x-1].style.fontWeight = "400";
         que[x-1].style.color="unset";
 
