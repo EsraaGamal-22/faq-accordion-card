adb2ce88beacd41a88b218b8709cbfb9bf5f8db4
esraagamal <esraagamal506@gmail.com>
2020-11-07 21:23:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-function showAns(x){
-    var arrow=document.getElementsByClassName("arrow-icon");
-    var que=document.getElementsByClassName("Q-content");
-    var ans=document.getElementsByClassName("answer");
-    if(window.getComputedStyle(ans[x-1]).display == 'none' )
-    {
+var questions = document.getElementsByClassName("question");
+var arrow=document.getElementsByClassName("arrow-icon");
+var que=document.getElementsByClassName("Q-content");
+var ans=document.getElementsByClassName("answer");
+// loop through each question
+for(i=0; i < questions.length; i++) {
+    console.log("for: "+"i: "+ i);
+    // add event listener to each question
+    questions[i].addEventListener("click", function(){
+        var index= this.id-1;
+        if(window.getComputedStyle(ans[index]).display == 'none' )
+        {
+            arrow[index].style.transform='rotate(360deg)';
+            ans[index].classList.add("active");
+            que[index].style.fontWeight = "700";
+            que[index].style.color="black";
+        }
+        else{
+            console.log("else: "+"index: "+ index);
 
-        arrow[x-1].style.transform='rotate(360deg)';
-        ans[x-1].classList.add("active");
-        que[x-1].style.fontWeight = "700";
-        que[x-1].style.color="black";
-    }
-    else{
-        arrow[x-1].style.transform='rotate(180deg)';
-        ans[x-1].classList.remove("active");
-        que[x-1].style.fontWeight = "400";
-        que[x-1].style.color="unset";
+            arrow[index].style.transform='rotate(180deg)';
+            ans[index].classList.remove("active");
+            que[index].style.fontWeight = "400";
+            que[index].style.color="unset";
 
+        }
+    });
+ 
+ }
 
-    }
-}
